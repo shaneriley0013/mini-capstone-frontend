4144448c8c762ecd590b789e564d83812333d117
SHANE RILEY <shaneriley0013@gmail.com>
2023-05-18 17:16:52 -0500

    tidy up console logs

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,6 +1,7 @@
 import axios from "axios";
 import { useState, useEffect } from "react";
 import { ProductsIndex } from "./ProductsIndex";
+// import { Modal } from "./Modal";
 import { Signup } from "./Signup";
 import { Login } from "./Login";
 import { Logout } from "./Logout";
@@ -11,7 +12,6 @@ export function Content() {
   const [products, setProducts] = useState([]);
 
   const handleIndexProducts = () => {
-    console.log("handleIndexProducts");
     axios.get("http://localhost:3000/products.json").then((response) => {
       console.log(response.data);
       setProducts(response.data);
