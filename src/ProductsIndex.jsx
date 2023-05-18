4a0545d46e2b267cabe18170b0f20cdcf0e908a3
SHANE RILEY <shaneriley0013@gmail.com>
2023-05-18 16:22:44 -0500

    add login/signup, eslint code, resize photos

--- src/ProductsIndex.jsx
+++ src/ProductsIndex.jsx
@@ -1,7 +1,15 @@
-export function ProductsIndex() {
+export function ProductsIndex(props) {
   return (
     <div>
       <h1>All Products</h1>
+      {props.products.map((product) => (
+        <div key={product.id}>
+          <h2>{product.name}</h2>
+          <p>Price: {product.price}</p>
+          <p>Description: {product.description}</p>
+          <img src={product.images[0] && product.images[0].url} />
+        </div>
+      ))}
     </div>
   );
 }
