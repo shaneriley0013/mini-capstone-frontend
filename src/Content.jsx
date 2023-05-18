f3b4024e4c2e49a95fad165e8644a44ad6cb4fdd
SHANE RILEY <shaneriley0013@gmail.com>
2023-05-18 10:59:49 -0500

    add code to set placeholder code

--- src/Content.jsx
+++ src/Content.jsx
@@ -1,7 +1,10 @@
+import { ProductsIndex } from "./ProductsIndex";
+
+
 export function Content() {
   return (
     <div>
-      <h1>Welcome to React!</h1>
+      <ProductsIndex />
     </div>
-  )
+  );
 }
